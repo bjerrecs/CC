74651b4cf2a76dce6a475545f16337a92ea154af
Simon Bjerre <bjerrecs@protonmail.com>
2021-11-17 20:45:13 +0100

    Fixed heroku build path

--- server.js
+++ server.js
@@ -279,10 +279,10 @@ app.post("/api/send_mail/maintenance", cors(), (req, res) => {
 /////////////////////////////////////////////////
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 });
 /////////////////////////////////////////////////
 
