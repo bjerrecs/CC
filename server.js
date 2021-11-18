bbeb01c936ff1f48db17067f9ebe800502a3279c
Simon Bjerre <bjerrecs@protonmail.com>
2021-11-18 21:22:28 +0100

    stackoverflow told me to do this

--- server.js
+++ server.js
@@ -279,7 +279,7 @@ app.post("/api/send_mail/maintenance", cors(), (req, res) => {
 /////////////////////////////////////////////////
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path_join(__dirname, '../client/build')));
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, './client/build', 'index.html'));
