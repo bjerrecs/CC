950ddf1e50055aa736b4ce5601db6a994f303a67
Simon Bjerre <bjerrecs@protonmail.com>
2021-11-20 20:42:20 +0100

    moved all imports to one file

---
+++ client/src/component.js
@@ -0,0 +1,51 @@
+import NavbarRoot from "./components/navbar";
+import Email from './pages/email';
+import Dashboard from "./pages/dashboard/dashboard";
+import Client from "./pages/client/client";
+import Clients from "./pages/client/clients";
+import ClientCreation from "./pages/client/client_creation";
+import ClientModify from "./pages/client/client_modify";
+import ApplicationPage from "./pages/docs/application";
+import UMPage from "./pages/docs/um";
+import SharesPage from "./pages/docs/shares";
+import LicensPage from "./pages/docs/licens";
+import PasswordsPage from "./pages/docs/passwords";
+import Warehouse from "./pages/warehouse/warehouse";
+import WarehouseLocation from "./pages/warehouse/locations";
+import WarehouseItems from "./pages/warehouse/items";
+import AccessPageView from "./pages/docs/access_page";
+import Profile from "./pages/profile/profile";
+import MaintenanceDashboard from "./pages/maintenance/dashboard";
+import MaintenanceShedule from "./pages/maintenance/schedule";
+import Login from "./pages/login";
+import PrivateRoute from "./private-route";
+import LoadingComponent from "./components/loading";
+import Sms from "./pages/sms";
+
+
+
+export {
+    SharesPage,
+    AccessPageView,
+    Warehouse,
+    WarehouseItems,
+    WarehouseLocation,
+    PasswordsPage,
+    LicensPage,
+    UMPage,
+    ApplicationPage,
+    NavbarRoot,
+    ClientModify,
+    Client,
+    Clients,
+    ClientCreation,
+    Email,
+    Sms,
+    Dashboard,
+    LoadingComponent,
+    PrivateRoute,
+    Login,
+    MaintenanceShedule,
+    MaintenanceDashboard,
+    Profile
+  }
--- client/src/rootrouter.js
+++ client/src/rootrouter.js
@@ -1,46 +1,35 @@
 import React from "react";
+
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
 
-/* IMPORT COMPONENTS*/
-import NavbarRoot from "./components/navbar";
-
-/* Import random pages*/
-import Email from './pages/email';
-import Dashboard from "./pages/dashboard/dashboard";
-
-/* Clients Imports*/
-import Client from "./pages/client/client";
-import Clients from "./pages/client/clients";
-import ClientCreation from "./pages/client/client_creation";
-import ClientModify from "./pages/client/client_modify";
-
-/* Client CC Pages Import*/
-import ApplicationPage from "./pages/docs/application";
-import UMPage from "./pages/docs/um";
-import SharesPage from "./pages/docs/shares";
-import LicensPage from "./pages/docs/licens";
-import PasswordsPage from "./pages/docs/passwords";
-
-/* Warehouse Imports*/
-import Warehouse from "./pages/warehouse/warehouse";
-import WarehouseLocation from "./pages/warehouse/locations";
-import WarehouseItems from "./pages/warehouse/items";
-import AccessPageView from "./pages/docs/access_page";
-
-/*import Profile from "./pages/profile/profile";*/
-import Profile from "./pages/profile/profile";
-import MaintenanceDashboard from "./pages/maintenance/dashboard";
-import MaintenanceShedule from "./pages/maintenance/schedule";
-import Login from "./pages/login";
-import PrivateRoute from "./private-route";
-import LoadingComponent from "./components/loading";
-import Sms from "./pages/sms";
-
-
+import { SharesPage,
+  AccessPageView,
+  Warehouse,
+  WarehouseItems,
+  WarehouseLocation,
+  PasswordsPage,
+  LicensPage,
+  UMPage,
+  ApplicationPage,
+  NavbarRoot,
+  ClientModify,
+  Client,
+  Clients,
+  ClientCreation,
+  Email,
+  Sms,
+  Dashboard,
+  LoadingComponent,
+  PrivateRoute,
+  Login,
+  MaintenanceShedule,
+  MaintenanceDashboard,
+  Profile 
+} from "./component"
 
 function RootRouter() {
   return (
