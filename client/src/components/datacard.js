9c0eef5864080875d931fa6efc19384159be8b6c
Simon Bjerre <bjerrecs@protonmail.com>
2021-12-03 17:53:51 +0100

    ui changes

--- client/src/components/datacard.js
+++ client/src/components/datacard.js
@@ -1,12 +1,21 @@
 
 import React from "react";
+import popover from 'react-bootstrap'
+import { GoInfo } from "react-icons/go";
 import './datacard.css'
-
 class Datacard extends React.Component {
+
+  
+
+  
     render() {
     return <div className="DatacardContainer">
-          <p className="DatacardTitle">{this.props.title}</p>
-          <p className="DatacardSubTitle">{this.props.subtitle}</p>
+            <b className="DatacardTitle">
+              {this.props.title} 
+            </b>
+            <p className="DatacardSubTitle">
+              {this.props.subtitle}
+            </p>
           <span>{this.props.component}</span>
       </div>
     }
