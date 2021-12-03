498f27ad58a2e72e6aec197adcc0dd2a1decf2e6
Simon Bjerre <bjerrecs@protonmail.com>
2021-12-03 13:26:04 +0100

    updated ux

--- client/src/components/add-one-item.js
+++ client/src/components/add-one-item.js
@@ -1,10 +1,12 @@
 import React, { useState} from 'react'
 import axios from 'axios';
 import { Button, OverlayTrigger, Tooltip, Form, Modal, Row, Col, FloatingLabel } from "react-bootstrap";
-import { GoPencil } from "react-icons/go";
+import { GoPencil,GoDiffAdded,GoDiffRemoved } from "react-icons/go";
 
 function UpdateOneItem(props)  {
+
     
+    const [itemUpdated, setItemUpdated] = useState(false)
     const [show, setShow] = useState(false);
     const [amountinput, setAmountInput] = useState("")
     const [restockamountinput, setRestockAmountInput] = useState("")
@@ -30,6 +32,7 @@ function UpdateOneItem(props)  {
     const handleSend = async() => {
         try {
             await axios.put('/api/warehouse/item/' + id, { amount: newAmount })
+            setItemUpdated(true)
         } catch (error) {
             console.log(error)
         }
@@ -39,9 +42,18 @@ function UpdateOneItem(props)  {
         console.log('New Restock: ' + restockamountinput)
         try {
             await axios.put('/api/warehouse/item/restock/' + id, { restockamount: newRestockAmount })
+            setItemUpdated(true)
         } catch (error) {
             console.log(error)
         }
+    }
+
+    if (itemUpdated) {
+        setTimeout(function(){
+            window.location.reload(false);
+        }, 500); 
+        // 1.5 sec delay before page reload
+
     }
 
     return (
@@ -62,26 +74,24 @@ function UpdateOneItem(props)  {
 
                     <Row className="g-2">
                         <Col md>
-                            <FloatingLabel label="New Amount">
+                            <FloatingLabel label="Restock Amount">
                                 <Form.Control type="number" value={amountinput} onChange={(e)   => setAmountInput(e.target.value)} />
                             </FloatingLabel>
                         </Col>
-                        <Col md>
-                            <Button className="fullheight" onClick={handleSend}>Update</Button>
-                        </Col>
+                        <Button className="fullheight" onClick={handleSend}>Update Amount</Button>
                     </Row>
 
+                    <br />
+                    <hr />
                     <br />
 
                     <Row className="g-2">
                         <Col md>
-                            <FloatingLabel label="New Restock Amount">
+                            <FloatingLabel label="Restock Amount">
                                 <Form.Control type="number" value={restockamountinput} onChange={(e) => setRestockAmountInput(e.target.value)}/>
                             </FloatingLabel>
                         </Col>
-                        <Col md>
-                            <Button className="fullheight" onClick={handleSendRestock}>Update</Button>
-                        </Col>
+                        <Button className="fullheight" onClick={handleSendRestock}>Update Restock Amount</Button>
                     </Row>
                     
                 </Modal.Body>
